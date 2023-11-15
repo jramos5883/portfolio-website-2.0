d38b22d63debec1a629ad2fe50323efa28e77e33
jramos5883 <jramos5883@gmail.com>
2023-11-14 23:10:17 -0800

    Going to work on the about me section

---
+++ portfolio-website-2.0-app/app/components/aboutMe/aboutMe.comp.jsx
@@ -0,0 +1,7 @@
+export default function AboutMe() {
+  return (
+    <div className="m-20">
+      <h1 className="text-3xl text-white">About Me!</h1>
+    </div>
+  );
+}
--- portfolio-website-2.0-app/app/page.js
+++ portfolio-website-2.0-app/app/page.js
@@ -1,9 +1,11 @@
 import Intro from "./components/intro/intro.comp";
+import AboutMe from "./components/aboutMe/aboutMe.comp";
 
 export default function Home() {
   return (
     <div className="">
       <Intro />
+      <AboutMe />
     </div>
   );
 }
